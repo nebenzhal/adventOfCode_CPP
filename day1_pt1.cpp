a3089bd32d014c8959fb31f66da7da80f803870d
nebenzhal <yonatan.nebenzhl@gmail.com>
2022-05-16 09:51:51 +0300

    day1_pt1

---
+++ day1_pt1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+int main(){
+    string line = "0";
+    string newLine;
+    int largerCount = -1;
+
+    ifstream infile ("inputs/input1.txt");
+
+    while(getline(infile,newLine)){
+        if((stoi(newLine))-(stoi(line)) > 0) largerCount++;
+        line = newLine;
+    }
+
+    infile.close();
+    cout << largerCount;
+}
